c8cf16fce5a2f94f06b41166b01b8a9780c23ac7
ryan-salomao <ryansalomao@gmail.com>
2022-10-18 17:48:18 -0300

    first commit

---
+++ Controllers/ComputerController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using MvcLabManager.Models;
+
+namespace MvcLabManager.Controllers;
+
+public class ComputerController : Controller
+{
+    private readonly LabManagerContext _context;
+
+    public ComputerController(LabManagerContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        return View(_context.Computers);
+    }
+
+    public IActionResult Show(int id)
+    {
+        Computer computer = _context.Computers.Find(id);
+
+        if(computer == null)
+        {
+            return NotFound();
+        }
+
+        return View(computer);
+    }
+}
